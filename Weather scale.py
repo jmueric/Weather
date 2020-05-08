d4ca1a6f639364cd4799cb05d02279a5d437b195
jmueric <59981920+jmueric@users.noreply.github.com>
2020-05-07 20:36:12 -0400

    Add files via upload

---
+++ Weather scale.py
@@ -0,0 +1,10 @@
+weather = int(input("Hows your weather? "))
+
+if weather >85:
+    print("It's too hot")
+elif weather >80:
+    print("Looking pretty")
+elif weather >65:
+    print("little chilly")    
+else:
+    print("It's too hot")
